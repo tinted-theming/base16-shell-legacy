3a54f4c23a320bdc3e80c663c0152d7cd2fb9e6a
Jamy Golden <jamy@jamygolden.com>
2022-06-24 19:19:58 +0200

    Use `$HOME` instead of `~` and change config path
    - Change config path to prevent littering the user's home dir. This path
      can be used for other projects too.

--- profile_helper.fish
+++ profile_helper.fish
@@ -1,8 +1,8 @@
 #!/usr/bin/env fish
 
-# [what] provides aliases for base16 themes and sets ~/.base16_theme
+# [what] provides aliases for base16 themes and sets $HOME/.base16_theme
 #
-# [usage] can be added to ~/.config/fish/config.fish like so:
+# [usage] can be added to $HOME/.config/fish/config.fish like so:
 #
 # if status --is-interactive
 #    source $HOME/.config/base16-shell/profile_helper.fish
@@ -16,13 +16,13 @@ if test -z $BASE16_SHELL
 end
 
 # load currently active theme...
-if test -e ~/.base16_theme
-  eval sh ~/.base16_theme
+if test -e "$HOME/.base16_theme"
+  eval sh "$HOME/.base16_theme"
 end
 
-if test -n "$BASE16_DEFAULT_THEME"; and not test -e ~/.base16_theme
+if test -n "$BASE16_DEFAULT_THEME"; and not test -e "$HOME/.base16_theme"
   ln -s "$BASE16_SHELL/scripts/base16-$BASE16_DEFAULT_THEME.sh" \
-    ~/.base16_theme
+    "$HOME/.base16_theme"
 end
 
 # set aliases, like base16_*...
@@ -31,10 +31,10 @@ for SCRIPT in $BASE16_SHELL/scripts/*.sh
   function $THEME -V SCRIPT -V THEME
     set partial_theme_name (string replace -a 'base16-' '' $THEME) # eg: ocean
     sh $SCRIPT
-    ln -sf $SCRIPT ~/.base16_theme
+    ln -sf $SCRIPT "$HOME/.base16_theme"
     set -gx BASE16_THEME (string split -m 1 '-' $THEME)[2]
-    if test -e ~/.tmux/plugins/base16-tmux
-      echo "set -g @colors-base16 '$partial_theme_name'" > ~/.tmux.base16.conf
+    if test -e "$HOME/.tmux/plugins/base16-tmux"
+      echo "set -g @colors-base16 '$partial_theme_name'" > "$HOME/.tmux.base16.conf"
     end
     if test (count $BASE16_SHELL_HOOKS) -eq 1; and test -d "$BASE16_SHELL_HOOKS"
       for hook in $BASE16_SHELL_HOOKS/*
@@ -44,4 +44,6 @@ for SCRIPT in $BASE16_SHELL/scripts/*.sh
   end
 end
 
-alias reset "command reset && [ -f ~/.base16_theme ] && sh ~/.base16_theme"
+alias reset "command reset \
+  && [ -f "$HOME/.base16_theme" ] \
+  && sh "$HOME/.base16_theme""
--- profile_helper.sh
+++ profile_helper.sh
@@ -15,8 +15,8 @@ _base16()
   local script=$1
   local theme=$2
   [ -f "$script" ] && . $script
-  ln -fs "$script" ~/.base16_theme
-  if [ -e ~/.tmux/plugins/base16-tmux ]; then echo -e "set -g \0100colors-base16 '$theme'" >| ~/.tmux.base16.conf; fi;
+  ln -fs "$script" "$HOME/.base16_theme"
+  if [ -e "$HOME/.tmux/plugins/base16-tmux" ]; then echo -e "set -g \0100colors-base16 '$theme'" >| ~/.tmux.base16.conf; fi;
   if [ -n ${BASE16_SHELL_HOOKS:+s} ] && [ -d "${BASE16_SHELL_HOOKS}" ]; then
     for hook in $BASE16_SHELL_HOOKS/*; do
       [ -f "$hook" ] && [ -x "$hook" ] && "$hook"
@@ -24,13 +24,15 @@ _base16()
   fi
 }
 
-if [ -n "$BASE16_DEFAULT_THEME" ] && [ ! -e ~/.base16_theme ]; then
-  ln -s "$BASE16_SHELL/scripts/base16-$BASE16_DEFAULT_THEME.sh" \
-    ~/.base16_theme
+if [ -n "$BASE16_DEFAULT_THEME" ] \
+  && [ ! -e "$HOME/.base16_theme" ]; then
+  ln -s \
+    "$BASE16_SHELL_PATH/scripts/base16-$BASE16_DEFAULT_THEME.sh" \
+    "$HOME/.base16_theme"
 fi
 
-if [ -e ~/.base16_theme ]; then
-  . ~/.base16_theme
+if [ -e "$HOME/.base16_theme" ]; then
+  . "$HOME/.base16_theme"
 fi
 
 # Set base16_* aliases
@@ -42,4 +44,4 @@ for script in "$BASE16_SHELL"/scripts/base16*.sh; do
   alias $func_name="_base16 \"${script}\" ${theme}"
 done;
 
-alias reset="command reset && [ -f ~/.base16_theme ] && . ~/.base16_theme"
+alias reset="command reset && [ -f $HOME/.base16_theme ] && . $HOME/.base16_theme"
